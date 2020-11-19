2f6bf8d824a32c806bb01b44fd952de2217b6765
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-18 22:47:50 -0300

    Este programa realiza a contagem regressiva de 200 a 100

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace contagem_regressiva200a100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int conta = 200; conta >= 100; conta--){
+                Console.WriteLine(conta);
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+    }
+}
